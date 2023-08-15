befa345e919c97041323ae3f0c278e9b28a7f7ab
Dawyla Maria <dawylamaria@gmail.com>
2023-08-15 13:06:58 -0300

    questao7

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+/* Q7 */
+
+using namespace std;
+int main(int argc, char** argv) {
+	
+	double lado;
+	
+	cout<< "Lado do quadrado:" << endl;
+	
+	cin >> lado;
+	
+	
+	cout<< "Area do quadrado:" << (lado * lado) * 2 << endl;
+	
+	
+	system("pause");
+	return 0;
+}
